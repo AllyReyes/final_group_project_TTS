c75c480839607a7b635493b5313ed6069a563504
Daniel Vazquez <dvazquez21@berkeley.edu>
2021-10-12 08:27:14 -0700

    Added secruity foundation/working on making user auth

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPosts/MethodSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.TechTalentBlog.BlogPosts;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+}
+
