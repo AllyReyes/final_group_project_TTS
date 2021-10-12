c4e7f316c6fda7caec7d571ae0d6e66e1ee55bb8
Daniel Vazquez <dvazquez21@berkeley.edu>
2021-10-12 09:09:58 -0700

    Limited non-logged in users from editing/deleting existing posts

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPosts/SecurityConfig.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPosts/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.techtalentsouth.TechTalentBlog.BlogPosts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception {
         security.httpBasic().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
